8040c3bcb6d9f60dee641bf6ff5a3d6bb19c9604
ctrlsoft-guest <ctrlsoft-guest>
2007-11-25 19:09:07 +0000

    Fix handling of releases.

--- debian/build-orig.sh
+++ debian/build-orig.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 # Build a source tarball for talloc
 
-samba4_repos=svn://svn.samba.org/samba/branches/SAMBA_4_0
+samba_repos=svn://svn.samba.org/samba
 version=$( dpkg-parsechangelog -l`dirname $0`/changelog | sed -n 's/^Version: \(.*:\|\)//p' | sed 's/-[0-9.]\+$//' )
 
 if echo $version | grep svn > /dev/null; then
 	# SVN Snapshot
 	revno=`echo $version | sed 's/^[0-9.]\+~svn//'`
-	svn export -r$revno $samba4_repos/source/lib/talloc talloc-$version
-	svn export -r$revno $samba4_repos/source/lib/replace talloc-$version/libreplace
+	svn export -r$revno $samba_repos/branches/SAMBA_4_0/source/lib/talloc talloc-$version
+	svn export -r$revno $samba_repos/branches/SAMBA_4_0/source/lib/replace talloc-$version/libreplace
 else
 	# Release
-	svn export $samba4_repos/../../tags/TALLOC_`echo $version | sed 's/\./_/g'` talloc-$version
+	svn export $samba_repos/tags/TALLOC_`echo $version | sed 's/\./_/g'` talloc-$version
 fi
 
 cd talloc-$version && ./autogen.sh && cd ..
