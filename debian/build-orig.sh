12b6b72c99bda3a996907f66ab02bdb65b7e07c9
Jelmer Vernooij <jelmer@samba.org>
2010-12-05 15:02:01 +0100

    Fix build-orig-sh.

--- debian/build-orig.sh
+++ debian/build-orig.sh
@@ -4,18 +4,13 @@ if [ -z "$GIT_URL" ]; then
 	GIT_URL=git://git.samba.org/samba.git
 fi
 
-TALLOCTMP=$TMPDIR/$RANDOM.talloc.git
+TALLOCTMP=`mktemp -d`
 git clone --depth 1 $GIT_URL $TALLOCTMP
-if [ ! -z "$REFSPEC" ]; then
-	pushd $TALLOCTMP
-	git checkout $REFSPEC || exit 1
-	popd
-fi
 pushd $TALLOCTMP/lib/talloc
 ./autogen-waf.sh
 ./configure
 make dist
 popd
 version=$( dpkg-parsechangelog -l`dirname $0`/changelog | sed -n 's/^Version: \(.*:\|\)//p' | sed 's/-[0-9.]\+$//' )
-mv $TALLOCTMP/lib/talloc/talloc-*.tar.gz talloc-$version.tar.gz
+mv $TALLOCTMP/lib/talloc/talloc-*.tar.gz talloc_$version.orig.tar.gz
 rm -rf $TALLOCTMP
